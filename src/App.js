ace4591491dc774bcd1b8e15383ee9c1a7111884
Chris Lee <clee@chicagoatlantic.com>
2025-04-24 16:46:01 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import IRChatbot from './CA-Chatbot';
+import IRChatbot from './ca-chatbot';
 
 function App() {
   return <IRChatbot />;
 }
 
 export default App;
+
+// import IRChatbot from './CA-Chatbot backup';
+
+// function App() {
+//   return <IRChatbot backup />;
+// }
+
+// export default App;
+
